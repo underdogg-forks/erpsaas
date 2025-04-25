fa65346c87824122697578c855a42d222283b683
Andrew Wallo <andrewdwallo@gmail.com>
2025-04-24 23:04:30 -0400

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
 use App\Http\Controllers\DocumentPrintController;
+use Filament\Facades\Filament;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect(Filament::getDefaultPanel()->getUrl());
 });
 
 Route::middleware(['auth'])->group(function () {
