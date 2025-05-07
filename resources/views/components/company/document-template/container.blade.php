01e7a32f7452aa74f2a999bddd35c13ba6380dec
Andrew Wallo <andrewdwallo@gmail.com>
2025-05-07 00:04:30 -0400

    wip

--- resources/views/components/company/document-template/container.blade.php
+++ resources/views/components/company/document-template/container.blade.php
@@ -5,7 +5,7 @@
 <div class="doc-template-container flex justify-center p-6">
     <div
         @class([
-            'doc-template-paper bg-[#ffffff] shadow-xl border border-gray-200',
+            'doc-template-paper bg-[#ffffff] shadow-xl ring-1 ring-gray-950/5 dark:ring-white/10',
             'w-full max-w-[820px] min-h-[1066px] max-h-[1200px] overflow-y-auto' => $preview === false,
             'w-[38.25rem] h-[49.5rem] overflow-hidden' => $preview === true,
         ])
